6bfd49fc6c3fca75431ec5fd0b9100b267e0354b
wei.yao <yaowei@goodsogood.com>
2020-12-14 22:17:09 +0800

    render props / children props

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -40,31 +40,99 @@ class Home extends Component {
 
     this.gotoHome = this.gotoHome.bind(this);
     this.ref1 = React.createRef();
+    this.state = {
+      count: 1
+    };
   }
 
   componentDidMount(previos) {
 
     console.log('==================>');
-    console.log(this.ref1);
+    console.log(this.state.count);
   }
 
   gotoHome() {
     this.props.history.push('/login');
   }
 
+  onChange() {
+    console.log(this.state.count)
+    // this.setState({
+    //   count: this.state.count++
+    // })
+
+    this.setState((state) => ({
+      count: ++state.count
+    }))
+  }
+
   render() {
+    let { count } = this.state;
     return (
       <>
         {/* <Wel ref={this.ref1} /> */}
-       
+
+        <Button user='test-namess' count={count}>{(s) => <div>{s.count}is meme---Children Props</div>}</Button>
 
 
-        <Header/>
-        <div className="p-home"><h1>Home page</h1></div>
+        <ButtonRender user='test-name' count={count} render={(tt) => (<div>{tt.count}------Render Props</div>)}></ButtonRender>
+
+        <Header />
+        <div className="p-home" onClick={() => this.onChange()}><h1>Home page</h1></div>
         <button onClick={this.gotoHome}>跳转Login</button>
       </>
     );
   }
+}
+
+/**
+ * 
+ * @param {*} props 测试children
+ */
+function Button(props) {
+  return (<div {...props}>hah ha ha ...{props.children(props)}</div>);
+}
+
+/**
+ * 
+ * @param {*} props  测试render props
+ */
+class ButtonRender extends React.Component {
+
+  static getDerivedStateFromProps() {
+    debugger;
+  }
+
+  shouldComponentUpdate(props, state) {
+    debugger;
+    return true;
+  }
+
+  // componentWillReceiveProps() {
+  //   debugger;
+  // }
+
+  componentDidMount() { debugger; }
+
+  componentDidUpdate() {
+    debugger;
+  }
+
+  componentDidCatch() {
+    debugger;
+  }
+
+  componentWillUnmount() {
+    debugger;
+  }
+
+  render() {
+    debugger;
+    let { render, ...rest } = this.props;
+
+    return (<div {...rest}>{render && render(rest)}</div>);
+  }
+
 }
 
 export default Home;
